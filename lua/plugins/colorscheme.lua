623fd804c21406e834b71ddc8c876386d1251eda
Onur Akdemir <onur.akdemir@icloud.com>
2023-01-25 01:37:35 +0300

    mocha

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,6 +7,16 @@ return {
     "catppuccin/nvim",
     lazy = true,
     name = "catppuccin",
+    opts = {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+    },
   },
   {
     "LazyVim/LazyVim",
