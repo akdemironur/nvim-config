b630da405491ef2a04717c5d2f5d05fb35753ae5
Onur Akdemir <onur.akdemir@icloud.com>
2023-01-31 00:36:53 +0300

    nord

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,31 +1,43 @@
 return {
-    -- {
-    --   "haishanh/night-owl.vim",
-    --   lazy = true,
-    -- },
+    {
+        "haishanh/night-owl.vim",
+        lazy = true,
+    },
     {
         "EdenEast/nightfox.nvim",
         lazy = true,
     },
-    -- {
-    --     "catppuccin/nvim",
-    --     lazy = true,
-    --     name = "catppuccin",
-    --     opts = {
-    --         flavour = "mocha", -- latte, frappe, macchiato, mocha
-    --         color_overrides = {
-    --             mocha = {
-    --                 base = "#000000",
-    --                 mantle = "#000000",
-    --                 crust = "#000000",
-    --             },
-    --         },
-    --     },
-    -- },
+    {
+        "nyoom-engineering/oxocarbon.nvim",
+        lazy = true,
+    },
+    {
+        "jacoborus/tender.vim",
+        lazy = true,
+    },
+    {
+        "arcticicestudio/nord-vim",
+        lazy = true,
+    },
+    {
+        "catppuccin/nvim",
+        lazy = true,
+        name = "catppuccin",
+        opts = {
+            flavour = "mocha", -- latte, frappe, macchiato, mocha
+            color_overrides = {
+                mocha = {
+                    base = "#000000",
+                    mantle = "#000000",
+                    crust = "#000000",
+                },
+            },
+        },
+    },
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "carbonfox",
+            colorscheme = "nord",
         },
     },
 }
