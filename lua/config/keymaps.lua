5eaebea37c6abe813f6ed937e5ae3a264f42159e
Onur Akdemir <onur.akdemir@icloud.com>
2023-02-01 00:01:51 +0300

    fuzzy finder hidden files keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local function map(mode, lhs, rhs, opts)
+    local keys = require("lazy.core.handler").handlers.keys
+    ---@cast keys LazyKeysHandler
+    -- do not create the keymap if a lazy keys handler exists
+    if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+        vim.keymap.set(mode, lhs, rhs, opts)
+    end
+end
+map("n", "<leader>fh", "<cmd>Telescope find_files hidden=true<cr>", { desc = "Find Files (hidden)" })
