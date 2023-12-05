1baa8f13a7c5508f1a969c2e0ef7f7598c9b1b71
Ubuntu <azureuser@CFD-EU-1.sd0fwdeg4qiuraihnr5jlhmhtb.gvxx.internal.cloudapp.net>
2023-12-05 22:09:18 +0000

    color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,82 +1,82 @@
 return {
-    {
-        "haishanh/night-owl.vim",
-        lazy = true,
+  {
+    "haishanh/night-owl.vim",
+    lazy = true,
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = true,
+  },
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+    lazy = true,
+  },
+  {
+    "jacoborus/tender.vim",
+    name = "tender",
+    lazy = true,
+  },
+  {
+    "arcticicestudio/nord-vim",
+    lazy = true,
+  },
+  {
+    "dracula/vim",
+    lazy = true,
+  },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+  },
+  {
+    "bluz71/vim-nightfly-colors",
+    name = "nightfly",
+    lazy = true,
+  },
+  {
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    lazy = true,
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = true,
+  },
+  {
+    "vv9k/bogster",
+    name = "bogster",
+    lazy = true,
+  },
+  {
+    "sainnhe/everforest",
+    name = "everforest",
+    lazy = true,
+  },
+  {
+    "cocopon/iceberg.vim",
+    name = "iceberg",
+    lazy = true,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    lazy = true,
+  },
+  {
+    "NTBBloodbath/sweetie.nvim",
+    name = "sweetie",
+    lazy = true,
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = true,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nightfox",
     },
-    {
-        "EdenEast/nightfox.nvim",
-        lazy = true,
-    },
-    {
-        "nyoom-engineering/oxocarbon.nvim",
-        lazy = true,
-    },
-    {
-        "jacoborus/tender.vim",
-        name = "tender",
-        lazy = true,
-    },
-    {
-        "arcticicestudio/nord-vim",
-        lazy = true,
-    },
-    {
-        "dracula/vim",
-        lazy = true,
-    },
-    {
-        "catppuccin/nvim",
-        lazy = true,
-        name = "catppuccin",
-    },
-    {
-        "bluz71/vim-nightfly-colors",
-        name = "nightfly",
-        lazy = true,
-    },
-    {
-        "bluz71/vim-moonfly-colors",
-        name = "moonfly",
-        lazy = true,
-    },
-    {
-        "ellisonleao/gruvbox.nvim",
-        lazy = true,
-    },
-    {
-        "vv9k/bogster",
-        name = "bogster",
-        lazy = true,
-    },
-    {
-        "sainnhe/everforest",
-        name = "everforest",
-        lazy = true,
-    },
-    {
-        "cocopon/iceberg.vim",
-        name = "iceberg",
-        lazy = true,
-    },
-    {
-        "rebelot/kanagawa.nvim",
-        name = "kanagawa",
-        lazy = true,
-    },
-    {
-        "NTBBloodbath/sweetie.nvim",
-        name = "sweetie",
-        lazy = true,
-    },
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        lazy = true,
-    },
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "rose-pine",
-        },
-    },
+  },
 }
